2e1c237d556ea7422cfe0816847eeca0b6b0d44c
CoryRodriguez <mrcoryrodriguez@gmail.com>
2018-02-13 20:33:44 -0600

    added local storage functionality

--- src/js/appes6.js
+++ src/js/appes6.js
@@ -53,6 +53,53 @@ class UI {
   }
 }
 
+// Local Storage Class
+class Store {
+  static getBooks(){
+    let books;
+    if(localStorage.getItem('books') === null){
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
+  }
+
+  static displayBooks(){
+    const books = Store.getBooks();
+
+    books.forEach(function (book) {
+      const ui = new UI;
+
+      // Add book to UI
+      ui.addBookToList(book);
+    });
+  }
+
+  static addBook(book){
+    const books = Store.getBooks();
+
+    books.push(book);
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+
+  static removeBook(isbn){
+    const books = Store.getBooks();
+
+    books.forEach(function (book, index) {
+      if(book.isbn === isbn){
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+
+// DOM load Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
+
 // Event Listener for addBook
 document.getElementById('book-form').addEventListener('submit', function (e) {
   // Get Form Values
@@ -74,15 +121,15 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
     // Add to Book List
     ui.addBookToList(book);
 
+    // Add to LS
+    Store.addBook(book);
+
     // Show Success
     ui.showAlert('Book Added!', 'success');
 
     // Clear Fields
     ui.clearFields();
   }
-
-
-
   e.preventDefault();
 });
 
@@ -92,7 +139,11 @@ document.getElementById('book-list').addEventListener('click', function (e) {
   // Instantiate UI
   const ui = new UI();
 
+  // Delete Book
   ui.deleteBook(e.target);
+
+  // Remove from LS
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
   //Show message
   ui.showAlert('Book Removed!', 'success');
